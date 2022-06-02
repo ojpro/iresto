a68ea05a07673c66b793646e943749d5bfbde11b
ojpro <oussamaeljabbari.mail@gmail.com>
2022-06-02 12:17:49 +0100

    Login bugs

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -31,6 +31,7 @@ class MainController extends Controller
 	public function login(Request $request)
 	{
 		$user = User::where('email', $request->email)->first();
+		return response()->json($user);
 		if (($user != null) && Hash::check($request->password, $user->password)) {
 			return response()->json('success');
 		} else {
